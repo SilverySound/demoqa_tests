e0e0ff85d249dd30abb70d5d3d4df0ffbc6642b9
Anna Shatokhina <anutik_94.08@mail.ru>
2024-03-20 23:48:38 +0300

    edits made

--- src/test/java/tests/FillFormTest.java
+++ src/test/java/tests/FillFormTest.java
@@ -4,7 +4,6 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
